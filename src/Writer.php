1f3f2069b33f16555db414c7502cb8ebb550eed3
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-02-19 01:13:00 +0100

    Added Writer

---
+++ src/Writer.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Indigo Csv package.
+ *
+ * (c) Indigo Development Team
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Indigo\Csv;
+
+use UnexpectedValueException;
+
+/**
+ * Csv Writer class
+ *
+ * Write data to CSV files
+ *
+ * @author Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ */
+class Write extends Csv
+{
+
+}
